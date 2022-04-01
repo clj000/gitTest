db81d7b55de8f685d00d7027dee932645a2e910e
achu <clj1118@163.com>
2022-04-01 10:37:41 +0800

    helloVersion1

---
+++ src/main/java/clj/HelloWorld.java
@@ -0,0 +1,12 @@
+package clj;
+
+/**
+ * @author achu
+ * @create 2022-04-01 10:35
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
